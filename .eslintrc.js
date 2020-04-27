d82e0b851836bb095e8f1a53396af52786399b19
ashakirzianov <ashakirzianov@icloud.com>
2020-04-27 17:20:19 -0300

    mongoose utils

--- .eslintrc.js
+++ .eslintrc.js
@@ -28,7 +28,7 @@ module.exports = {
       },
       "singleline": {
         "delimiter": "comma",
-        "requireLast": true
+        "requireLast": false
       },
     }],
     "@typescript-eslint/explicit-function-return-type": "off",
