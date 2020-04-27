e0e879a64c2a4f5d49912b32a6fd594be2940e16
ashakirzianov <ashakirzianov@icloud.com>
2020-04-27 20:38:08 -0300

    Config dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { ApolloServer } from 'apollo-server';
+import { config as configEnv } from 'dotenv';
 import { typeDefs, resolvers } from './graphql';
 import { log } from './utils';
+
+configEnv();
+startup();
 
 async function startup() {
     const server = new ApolloServer({ typeDefs, resolvers });
     const { url } = await server.listen();
     log(`Server ready at ${url}`);
 }
-
-startup();
