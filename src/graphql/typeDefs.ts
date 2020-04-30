0f4184a519340c2513c3ccfdfb0d6579634a5ade
ashakirzianov <ashakirzianov@icloud.com>
2020-04-30 14:27:44 -0300

    Put schema back in gql tag

--- src/graphql/typeDefs.ts
+++ src/graphql/typeDefs.ts
@@ -1,5 +1,61 @@
 import { gql } from 'apollo-server';
-import { importSchema } from 'graphql-import';
+export const typeDefs = gql`
+type Query {
+    auth(token: String, provider: String): AuthToken
+    search(query: String): [Card]
+    bookmarks(booqId: BooqIdInput): [Bookmark]
+    highlights(booqId: BooqIdInput): [Highlight]
+}
+type Mutation {
+    addBookmark(bm: BookmarkInput): HasUuid
+    addHighlight(hl: HighlightInput): HasUuid
+}
 
-const schemaContent = importSchema('src/graphql/schema.graphql');
-export const typeDefs = gql`${schemaContent}`;
+input BooqIdInput {
+    id: String!
+    source: String!
+}
+input BookmarkInput {
+    booqId: BooqIdInput
+    path: [Int!]!
+    uuid: String
+}
+input HighlightInput {
+    booqId: BooqIdInput
+    group: String!
+    start: [Int!]
+    end: [Int!]
+    uuid: String!
+}
+
+type HasUuid {
+    uuid: String
+}
+type BooqId {
+    source: String!
+    id: String!
+}
+type BooqRange {
+    start: [Int!]
+    end: [Int!]
+}
+
+type Bookmark {
+    uuid: String
+    booqId: BooqId
+    path: [Int!]
+}
+type Highlight {
+    uuid: String
+    booqId: BooqId
+    range: BooqRange
+    group: String
+}
+type Card {
+    title: String
+    author: String
+}
+type AuthToken {
+    token: String
+}
+`;
