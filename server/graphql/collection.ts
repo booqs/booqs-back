829efc82a6626c60998d39f5f2f88cf34d1122bd
ashakirzianov <ashakirzianov@icloud.com>
2020-05-13 16:57:42 -0300

    Fix collection resolver

--- server/graphql/collection.ts
+++ server/graphql/collection.ts
@@ -1,12 +1,14 @@
 import { IResolvers } from 'apollo-server';
+import { filterUndefined } from '../../core';
 import { forIds } from '../books';
 import { BooqParent } from './booq';
 
 export type CollectionParent = string[];
 export const collectionResolver: IResolvers<CollectionParent> = {
     Collection: {
-        async booqs(parent): Promise<Array<BooqParent | undefined>> {
-            return forIds(parent);
+        async booqs(parent): Promise<BooqParent[]> {
+            return forIds(parent)
+                .then(filterUndefined);
         },
     },
 };
