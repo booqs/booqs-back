5998c6a8f497a72a3a186330bb0dcf1e28b38c8c
ashakirzianov <ashakirzianov@icloud.com>
2020-05-17 12:41:39 -0300

    Minor fix

--- server/images/index.ts
+++ server/images/index.ts
@@ -1,21 +1,7 @@
 import { Booq } from '../../core';
-import { booqForId } from '../books';
 import { uploadBooqImages } from './upload';
 import { isAlreadyPrepared, markAsPrepared } from './mark';
 export { booqImageUrl } from './upload';
-
-export async function prepareImages(booqId: string) {
-    if (await isAlreadyPrepared(booqId)) {
-        return;
-    }
-
-    const booq = await booqForId(booqId);
-    if (booq) {
-        await uploadBooqImages(booq, booqId);
-    }
-
-    markAsPrepared(booqId);
-}
 
 export async function prepareImagesWithBooq(booqId: string, booq: Booq) {
     if (await isAlreadyPrepared(booqId)) {
