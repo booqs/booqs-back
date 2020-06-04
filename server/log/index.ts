cda9d07e406e6ada197be49dd4425c7ba91f5c1e
ashakirzianov <ashakirzianov@icloud.com>
2020-06-03 22:51:53 -0300

    Implement problem logging to db

---
+++ server/log/index.ts
@@ -0,0 +1,28 @@
+import { typedModel, TypeFromSchema } from '../mongoose';
+
+const schema = {
+    id: {
+        type: String,
+        required: true,
+    },
+    kind: {
+        type: String,
+        required: true,
+    },
+    message: String,
+    data: Object,
+} as const;
+
+export type DbLog = TypeFromSchema<typeof schema>;
+const logModel = typedModel('log', schema);
+
+export async function logItem(item: DbLog) {
+    return logModel.insertMany([item]);
+}
+
+export async function logExists({ kind, id }: {
+    kind: string,
+    id: string,
+}) {
+    return logModel.exists({ kind, id });
+}
