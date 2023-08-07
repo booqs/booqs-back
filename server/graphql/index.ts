e6a6edb2e8b5b7bd152cb52f346ae50181043d0a
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-08-07 16:31:52 -0700

    Move 'resolvers' to the separate file

--- server/graphql/index.ts
+++ server/graphql/index.ts
@@ -1,25 +1,3 @@
-import { IResolvers } from '@graphql-tools/utils'
-import GraphQLJSON from 'graphql-type-json'
-import { queryResolver } from './query'
-import { mutationResolver } from './mutation'
-import { collectionResolver } from './collection'
-import { booqResolver } from './booq'
-import { booqHistoryResolver } from './history'
-import { bookmarkResolver } from './bookmark'
-import { highlightResolver } from './highlight'
-import { userResolver } from './user'
-
 export { readTypeDefs } from './typeDefs'
 export { context } from './context'
-
-export const resolvers: IResolvers = {
-    ...queryResolver,
-    ...mutationResolver,
-    ...collectionResolver,
-    ...booqResolver,
-    ...booqHistoryResolver,
-    ...bookmarkResolver,
-    ...highlightResolver,
-    ...userResolver,
-    BooqNode: GraphQLJSON,
-}
+export { resolvers } from './resolvers'
---
+++ server/graphql/resolvers.ts
@@ -0,0 +1,22 @@
+import { IResolvers } from '@graphql-tools/utils'
+import GraphQLJSON from 'graphql-type-json'
+import { queryResolver } from './query'
+import { mutationResolver } from './mutation'
+import { collectionResolver } from './collection'
+import { booqResolver } from './booq'
+import { booqHistoryResolver } from './history'
+import { bookmarkResolver } from './bookmark'
+import { highlightResolver } from './highlight'
+import { userResolver } from './user'
+
+export const resolvers: IResolvers = {
+    ...queryResolver,
+    ...mutationResolver,
+    ...collectionResolver,
+    ...booqResolver,
+    ...booqHistoryResolver,
+    ...bookmarkResolver,
+    ...highlightResolver,
+    ...userResolver,
+    BooqNode: GraphQLJSON,
+}
