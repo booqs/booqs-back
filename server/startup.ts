217ffdc4aa5b79f433987aefe1c904b712f400bd
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-08-14 23:20:27 -0700

    Temp cors logging

--- server/startup.ts
+++ server/startup.ts
@@ -20,8 +20,10 @@ export async function startup() {
         origin(origin, callback) {
             // TODO: disallow undefined origin?
             if (!origin || origin?.endsWith('booqs.app') || origin?.endsWith('localhost:3000')) {
+                console.log(`CORS: ${origin} is allowed by CORS`)
                 callback(null, true)
             } else {
+                console.log(`CORS: ${origin} is not allowed by CORS`)
                 callback(new Error(`${origin} is not allowed by CORS'`))
             }
         },
