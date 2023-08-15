d898cc370898238680128d6ad42f53f50dd25ceb
Anton Shakirzianov <ashakirzianov@icloud.com>
2023-08-14 21:44:25 -0700

    Return correct cover url after upload

--- server/upload.ts
+++ server/upload.ts
@@ -4,6 +4,7 @@ import { fromCookie } from './auth'
 import { uploadToSource } from './books'
 import { addToCollection } from './users/collections'
 import { parseCookies } from './cookie'
+import { booqImageUrl } from './images'
 
 export const UPLOADS_COLLECTION = 'uploads'
 export function addUploadHandler(app: Express, route: string) {
@@ -32,6 +33,9 @@ export function addUploadHandler(app: Express, route: string) {
             let added = addToCollection(user.id, UPLOADS_COLLECTION, id)
             if (!added) {
                 console.error('Failed to add upload to collection')
+            }
+            if (cover) {
+                cover = booqImageUrl(id, cover)
             }
             return res.status(200).send({
                 id, title, cover,
